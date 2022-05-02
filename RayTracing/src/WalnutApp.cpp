845cff8b430b8ccfd400becd3fcf86c8cc63c055
d_koltun <d_koltun@wargaming.net>
2022-05-02 18:35:27 +0300

    Draw background & red sphere at the center

--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -5,6 +5,8 @@
 #include "Walnut/Random.h"
 #include "Walnut/Timer.h"
 
+#include "ray.hpp"
+
 #include <glm/glm.hpp>
 
 #include <iostream>
@@ -13,10 +15,43 @@
 
 using namespace Walnut;
 
+using color = glm::vec3;
+using point3 = glm::vec3;
+using vec3 = glm::vec3;
+
 namespace
 {
 constexpr std::string_view C_IMAGE_PATH = "image.ppm";
+
+void write_color(std::ostream& out, color pixel_color) {
+	// Write the translated [0,255] value of each color component.
+	out << static_cast<int>(255.999 * pixel_color.x) << ' '
+		<< static_cast<int>(255.999 * pixel_color.y) << ' '
+		<< static_cast<int>(255.999 * pixel_color.z) << '\n';
 }
+
+bool hitSphere(const point3& center, float radius, const Ray& r)
+{
+	vec3 oc = r.origin() - center;
+	auto a = glm::dot(r.direction(), r.direction());
+	auto b = 2 * glm::dot(r.direction(), oc);
+	auto c = glm::dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+
+	return discriminant > 0;
+}
+
+vec3 rayColor(const Ray& ray)
+{
+	if (hitSphere(point3(0.f, 0.f, -1.f), 0.5, ray))
+		return color(1.f, 0.f, 0.f);
+
+	vec3 unitDirection = glm::normalize(ray.direction());
+	auto t = 0.5f * (unitDirection.y + 1.f);
+	return (1.f - t) * color(1.f, 1.f, 1.f) + t * color(0.5f, 0.7f, 1.f); //-- lerp
+}
+}
+
 
 class ExampleLayer : public Walnut::Layer
 {
@@ -40,8 +75,17 @@ public:
 
 		ImGui::Begin("Viewport");
 		
-		m_viewportWidth = static_cast<uint32_t>(ImGui::GetContentRegionAvail().x);
-		m_viewportHeight = static_cast<uint32_t>(ImGui::GetContentRegionAvail().y);
+		m_imageWidth = static_cast<uint32_t>(ImGui::GetContentRegionAvail().x);
+		m_imageHeight = static_cast<uint32_t>(ImGui::GetContentRegionAvail().y);
+		m_aspectRatio = (float) m_imageWidth / m_imageHeight;
+		m_viewportHeight = 2.f;
+		m_viewportWidth = m_aspectRatio * m_viewportHeight;
+		m_focalLength = 1.f;
+
+		m_origin = vec3(0.f, 0.f, 0.f);
+		m_horizontal = vec3(m_viewportWidth, 0.f, 0.f);
+		m_vertical = vec3(0.f, m_viewportHeight, 0.f);
+		m_lowerLeftCorner = m_origin - m_horizontal / 2.f - m_vertical / 2.f - vec3(0.f, 0.f, m_focalLength);
 
 		if (m_image)
 			ImGui::Image(m_image->GetDescriptorSet(), { (float)m_image->GetWidth(), (float)m_image->GetHeight()});
@@ -57,7 +101,7 @@ public:
 
 		glm::vec3 v;
 
-		if (!m_image || m_viewportWidth != m_image->GetWidth() || m_viewportHeight != m_image->GetHeight())
+		if (!m_image || m_imageWidth != m_image->GetWidth() || m_imageHeight != m_image->GetHeight())
 		{
 			renderImageToFile(C_IMAGE_PATH);
 			readImageFromFile(C_IMAGE_PATH);
@@ -76,33 +120,32 @@ private:
 
 		if (out.is_open())
 		{
-			const int width = m_viewportWidth;
-			const int height = m_viewportHeight;
+			const int width = m_imageWidth;
+			const int height = m_imageHeight;
 
 			out << "P3\n" << width << ' ' << height << "\n255\n";
 
 			for (int j = height - 1; j >= 0; --j)
 			{
+				std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 				for (int i = 0; i < width; ++i)
 				{
-					auto r = double(i) / (width - 1);
-					auto g = double(j) / (height - 1);
-					auto b = 0.25;
+					auto u = float(i) / (width - 1); //-- 0 -> 1
+					auto v = float(j) / (height - 1); //-- 0 -> 1
+					
+					Ray r(m_origin, m_lowerLeftCorner + u * m_horizontal + v * m_vertical - m_origin);
+					color pixelColor = rayColor(r);
 
-					int ir = static_cast<int>(255.999 * r);
-					int ig = static_cast<int>(255.999 * g);
-					int ib = static_cast<int>(255.999 * b);
-
-					out << ir << ' ' << ig << ' ' << ib << '\n';
+					write_color(out, pixelColor);
 				}
 			}
 
+			std::cerr << "\nDone.\n";
 			out.close();
 		}
 
 		m_writeTime = timer.ElapsedMillis();
 	}
-
 	void readImageFromFile(std::string_view path)
 	{
 		Timer timer;
@@ -155,12 +198,22 @@ private:
 private:
 	std::shared_ptr<Image> m_image;
 	uint32_t* m_imageData = nullptr;
-	uint32_t m_viewportWidth = 0;
-	uint32_t m_viewportHeight = 0;
+	uint32_t m_imageWidth = 0;
+	uint32_t m_imageHeight = 0;
 
 	float m_writeTime = 0.f;
 	float m_readTime = 0.f;
 	float m_lastRenderTime = 0.f;
+
+	//-- camera
+	float m_aspectRatio;
+	float m_viewportWidth;
+	float m_viewportHeight;
+	float m_focalLength;
+	vec3 m_origin;
+	vec3 m_horizontal;
+	vec3 m_vertical;
+	vec3 m_lowerLeftCorner;
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
---
+++ RayTracing/src/ray.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+class Ray
+{
+public:
+	Ray(const glm::vec3& origin, const glm::vec3& direction)
+		: m_origin(origin)
+		, m_direction(direction) {}
+
+	const glm::vec3& origin() const { return m_origin; }
+	const glm::vec3& direction() const { return m_direction; }
+
+	glm::vec3 at(float t) const { return m_origin + m_direction * t; }
+
+private:
+	glm::vec3 m_origin;
+	glm::vec3 m_direction;
+};
